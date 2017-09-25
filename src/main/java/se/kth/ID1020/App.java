f7547dc416dc932074c4abd529538b0b30eb50b5
Baz <sebastian.forsmark@gmail.com>
2017-09-25 23:38:07 +0200

    blegh

--- src/main/java/se/kth/ID1020/App.java
+++ src/main/java/se/kth/ID1020/App.java
@@ -4,21 +4,20 @@ package se.kth.ID1020;
 public class App {
     public static void main(String[] args) {
         Trie root = new Trie();
+        root.put("app");
+        root.put("apple");
         root.put("apple");
         root.put("applepie");
-        root.put("applesauce");
-        root.put("applep");
-/*        System.out.println(root.get("apple"));
-        System.out.println(root.get("app"));
-        System.out.println(root.get("ap"));
-        System.out.println(root.count("app"));
-        System.out.println(root.distinct("apple"));*/
 
-        TrieIterator itr = new TrieIterator(root, "apple");
+        System.out.println("There are 4 words put into the Trie; 'App', 2 'Apple's, and 'ApplePie'");
+        System.out.println("count 'app': " + root.count("app"));
+        System.out.println("get 'apple': " + root.get("apple"));
+        System.out.println("distinct 'apple': " + root.distinct("apple"));
 
-while (itr.hasNext())
-        System.out.println(itr.next().key);
-
+        TrieIterator itr = new TrieIterator(root, "app");
+        System.out.println("Iterate past the prefix app: ");
+        while (itr.hasNext())
+            System.out.println(itr.next().key);
 
 
     }
